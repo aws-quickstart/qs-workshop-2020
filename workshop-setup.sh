8728d9bae7b8e201e6e914b0082ceb111371e9b8
troy-ameigh <ameighta@amazon.com>
2020-06-22 11:20:14 -0500

    Added new README files and a script to create the workshop repo.

--- workshop-setup.sh
+++ workshop-setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-CURRENTDIR=${pwd}
+CURRENTDIR=$(pwd)
 
 echo $CURRENTDIR
 
 
-# step 1: name of the remote repo. Enter a SINGLE WORD ..or...separate with hyphens
-echo "Name Repo for Workshop (I.E> Workshop-<Date>)?"
+# step 1: name of the remote repo you created under "https://github.com/aws-quickstart". Enter a SINGLE WORD ..or...separate with hyphens
+echo "Name Repo for Workshop (I.E> Repo name from step #1 in README):"
 read REPO_NAME
 
 
@@ -14,23 +14,23 @@ read REPO_NAME
 echo "What is the absolute path to your local project directory?"
 read PROJECT_PATH
 
-mkdir -p '$PROJECT_PATH/$REPO_NAME'
-
-#echo "What is your github username?"
+#NOt USED - echo "What is your github username?"
 #read USERNAME
 
-# step 3 : go to path
-cd $PROJECT_PATH
+# step 3 : create empty repo
+cd ${PROJECT_PATH}
+git init ${REPO_NAME}
 
+LCLREPO=${PROJECT_PATH}/${REPO_NAME}
 
-# step 4: initialise the repo locally, create blank README, add and commit
-git init
-cp $CURRENTDIR/docs/content/README.adoc "$PROJECT_PATH"/README.adoc
+# step 4: goto local repo and copy in files, add and commit
+cd $LCLREPO
 mkdir -p docs/content
-cp $CURRENTDIR/docs/content/*.yaml $PROJECT_PATH/docs/content/
-cp $CURRENTDIR/docs/content/.taskcat.yaml $PROJECT_PATH/docs/content/
-cp $CURRENTDIR/docs/code_snippets.txt $PROJECT_PATH/docs/
-cp $CURRENTDIR/docs/*.pptx $PROJECT_PATH/docs/
+cp $CURRENTDIR/docs/content/README.adoc $LCLREPO/README.adoc
+cp $CURRENTDIR/docs/content/*.yaml $LCLREPO/docs/content/
+cp $CURRENTDIR/docs/content/.taskcat.yaml $LCLREPO/docs/content/
+cp $CURRENTDIR/docs/code_snippets.txt $LCLREPO/docs/
+cp $CURRENTDIR/docs/*.pptx $LCLREPO/docs/
 git add .
 git commit -m 'initial commit -setup with .sh script'
 
@@ -39,11 +39,11 @@ git commit -m 'initial commit -setup with .sh script'
 #curl -u ${USERNAME} https://api.github.com/user/repos -d "{\"name\": \"${REPO_NAME}\", \"description\": \"${DESCRIPTION}\"}"
 
 #  step 6 add the remote github repo to local repo and push
-#git remote add origin https://github.com/aws-quickstart/${REPO_NAME}.git
-#git push --set-upstream origin master
+git remote add origin https://github.com/aws-quickstart/${REPO_NAME}.git
+git push --set-upstream origin master
 
 # step 7 change to your project's root directory.
-cd "$PROJECT_PATH"
+cd $LCLREPO
 
 echo "Done. Go to https://github.com/aws-quickstart/$REPO_NAME to see."
 echo " *** You're now in your project root. ***"
